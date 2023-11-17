f7137b043b79d995ed6cf2994047ab6a40a2ea52
BehzodHamroyev <behzodhamroyev007@gmail.com>
2023-11-17 10:01:19 +0500

    Merged main | Behzod

--- src/shared/ui/SearchTitle/ui/SerchTile.tsx
+++ src/shared/ui/SearchTitle/ui/SerchTile.tsx
@@ -1,7 +1,6 @@
-import React, { memo } from 'react'
-import cls from './SearchTitle.module.scss'
+import React, { memo } from 'react';
+import cls from './SearchTitle.module.scss';
+
 export const SerchTile = memo(() => {
-  return (
-    <div className={}>SerchTile</div>
-  )
-})
+  return <div className={cls.searchTitle}>SerchTile</div>;
+});
