6321fbf19f67192b0b5bd20b29df80efb490fdde
Jasurbek Isokov <jasurisoqov685@gmail.com>
2023-11-20 18:07:22 +0500

    Added Skeleton in Reading page

--- src/pages/ReadingPage/ui/ReadingPage.tsx
+++ src/pages/ReadingPage/ui/ReadingPage.tsx
@@ -1,14 +1,18 @@
 import React, { useContext } from 'react';
+import { useSelector } from 'react-redux';
 import cls from './ReadingPage.module.scss';
+import cls2 from '../../../entities/ReadingTranskriptLotin/ui/ReadingTranskriptLotin.module.scss';
 import { classNames } from '@/shared/lib/classNames/classNames';
 
 // ---------- | | ---------- //
 import { Navbar } from '@/widgets/Nabar';
 import { Sidebar } from '@/widgets/Sidebar';
+import { getIsLoading } from '@/pages/MainPage';
 import { ReadingNavbar } from '@/widgets/ReadingNavbar';
 import { ReadingSidebar } from '@/widgets/ReadingSidebar';
 // import { ReadingArabic } from '@/entities/ReadingArabic';
 import { ButtonsContext } from '@/shared/lib/context/ButtonsContext';
+import BookBoxSkeleton from '@/shared/ui/BookBoxSkeleton/BookBoxSkeleton';
 import { ReadingTranskriptLotin } from '@/entities/ReadingTranskriptLotin';
 import { ReadingPrevNextBtnGroup } from '@/shared/ui/ReadingPrevNextBtnGroup';
 import { ReadingPrevNextSuraBtnGroup } from '@/shared/ui/ReadingPrevNextSuraBtnGroup';
@@ -20,6 +24,7 @@ interface ReadingPageProps {
 const ReadingPage = (props: ReadingPageProps) => {
   const { className } = props;
   const { readingSidebarActive } = useContext(ButtonsContext);
+  const isLoading = useSelector(getIsLoading);
 
   return (
     <div
@@ -43,11 +48,27 @@ const ReadingPage = (props: ReadingPageProps) => {
       >
         {/* <ReadingArabic /> */}
 
-        <ReadingTranskriptLotin />
+        {isLoading ? (
+          <div
+            className={classNames(cls2.ReadingTranskriptLotin, {}, [className])}
+          >
+            <div
+              className={classNames(cls2.ReadingTranskriptLotin__readBox, {}, [
+                className,
+              ])}
+            >
+              <BookBoxSkeleton />
+            </div>
+          </div>
+        ) : (
+          <>
+            <ReadingTranskriptLotin />
 
-        <ReadingPrevNextBtnGroup />
+            <ReadingPrevNextBtnGroup />
 
-        <ReadingPrevNextSuraBtnGroup />
+            <ReadingPrevNextSuraBtnGroup />
+          </>
+        )}
       </div>
     </div>
   );
