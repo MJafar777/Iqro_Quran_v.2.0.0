b2aa08e9302d2e8cd7e511c164ddcfbd6d2f6138
MJafar777 <mirzaraximovjafar2001@gmail.com>
2023-11-20 15:21:09 +0500

    create new page surah info

---
+++ src/pages/InformationPage/ui/InfoSurahPage.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+
+import cls from './InformationPage.module.scss';
+import { classNames } from '@/shared/lib/classNames/classNames';
+
+const InfoSurahPage = () => {
+  return (
+    <div className={classNames(cls.InfoSurahPage, {}, [])}>
+      InfoSurahPage Lorem ipsum dolor sit amet consectetur adipisicing elit.
+      Voluptatem voluptas reprehenderit porro vitae incidunt sint sed nihil
+      exercitationem quis illo, accusamus autem nobis amet dolor recusandae
+      animi quisquam consectetur? Animi ex cum ipsam! Qui beatae adipisci eaque
+      ea ipsum sapiente, deserunt incidunt inventore esse, veritatis doloremque
+      ullam rem reprehenderit quo consectetur voluptatum eos nostrum. Error
+      aperiam autem doloribus, quod vitae, nulla nostrum consectetur suscipit
+      ipsa quia quaerat asperiores. Voluptatem, commodi! Autem assumenda
+      aperiam, dolorum doloremque praesentium voluptates adipisci itaque
+      accusantium aliquam, dolore culpa inventore officia corporis minus
+      asperiores mollitia obcaecati repellendus perferendis cum, porro eaque
+      similique. Aut illum est hic?
+    </div>
+  );
+};
+
+export default InfoSurahPage;
