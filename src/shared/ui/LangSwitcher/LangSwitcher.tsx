669868b2cbb89d9ff0288a912e673a973d70df4a
BehzodHamroyev <behzodhamroyev007@gmail.com>
2023-11-24 16:00:11 +0500

    lange switcher fixed

--- src/shared/ui/LangSwitcher/LangSwitcher.tsx
+++ src/shared/ui/LangSwitcher/LangSwitcher.tsx
@@ -1,79 +1,46 @@
 import * as React from 'react';
-import Box from '@mui/material/Box';
-import MenuItem from '@mui/material/MenuItem';
-import Menu from '@mui/material/Menu';
 import { useTranslation } from 'react-i18next';
 import { Lang } from '@/shared/assets/icons/navbar';
 
 import cls from './LangSwitcher.module.scss';
 
 export const LangSwitcher = React.memo(() => {
-  const [anchorEl, setAnchorEl] = React.useState<null | HTMLElement>(null);
-  const [mobileMoreAnchorEl, setMobileMoreAnchorEl] =
-    React.useState<null | HTMLElement>(null);
-  const isMenuOpen = Boolean(anchorEl);
   const { i18n } = useTranslation();
-
-  const handleProfileMenuOpen = (event: React.MouseEvent<HTMLElement>) => {
-    setAnchorEl(event.currentTarget);
-  };
-
-  const handleMobileMenuClose = () => {
-    setMobileMoreAnchorEl(null);
-  };
-
+  const [isOpenMenu, setIsOpenMenu] = React.useState(false);
   const toggle = async (lang: string) => {
     i18n.changeLanguage(lang);
+    setIsOpenMenu(false);
   };
 
-  const handleMenuClose = () => {
-    setAnchorEl(null);
-    handleMobileMenuClose();
-  };
-
-  const menuId = 'primary-search-account-menu';
   const renderMenu = (
-    <Menu
-      anchorEl={anchorEl}
-      // anchorOrigin={{
-      //   vertical: 'top',
-      //   horizontal: 'right',
-      // }}
-      id={menuId}
-      keepMounted
-      transformOrigin={{
-        vertical: 'top',
-        horizontal: 'right',
-      }}
-      className={cls.modalLang}
-      open={isMenuOpen}
-      onClose={handleMenuClose}
-    >
-      <MenuItem
+    <div className={cls.menu}>
+      <div
+        className={cls.menuItem}
         onClick={() => {
-          handleMenuClose();
           toggle('uz');
         }}
       >
         Latin
-      </MenuItem>
-      <MenuItem
+      </div>
+      <div
+        className={cls.menuItem}
         onClick={() => {
-          handleMenuClose();
           toggle('kr');
         }}
       >
         Крилл
-      </MenuItem>
-    </Menu>
+      </div>
+    </div>
   );
 
+  // const modalLangList=(<div></div>)
+
   return (
-    <Box sx={{ flexGrow: 1 }}>
-      <div onClick={handleProfileMenuOpen}>
+    <div className={cls.containerLang}>
+      <div onClick={() => setIsOpenMenu((pre) => !pre)} className={cls.icon}>
         <Lang className={cls.icon} />
       </div>
-      {renderMenu}
-    </Box>
+      {isOpenMenu ? renderMenu : ''}
+    </div>
   );
 });
