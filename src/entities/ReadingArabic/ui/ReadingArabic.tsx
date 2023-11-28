04342a53d66fcaf4c327f26d450d5a5a46ed148f
Jasurbek Isokov <jasurisoqov685@gmail.com>
2023-11-28 11:04:35 +0500

    Added Quran fonts

--- src/entities/ReadingArabic/ui/ReadingArabic.tsx
+++ src/entities/ReadingArabic/ui/ReadingArabic.tsx
@@ -1,25 +1,24 @@
-import { memo, useEffect } from 'react';
-import { useSelector } from 'react-redux';
+import { memo } from 'react';
+// import { useSelector } from 'react-redux';
 import cls from './ReadingArabic.module.scss';
 import { classNames } from '@/shared/lib/classNames/classNames';
-import {
-  getReadingArabicData,
-  getReadingArabicError,
-  getReadingArabicIsLoading,
-} from '../model/selectors/readingArabic';
+// import {
+//   getReadingArabicData,
+//   getReadingArabicError,
+//   getReadingArabicIsLoading,
+// } from '../model/selectors/readingArabic';
 
 import {
   ReducersList,
   DynamicModuleLoader,
 } from '@/shared/lib/components/DynamicModuleLoader/DynamicModuleLoader';
 import { readingArabicReducer } from '../model/slice/readingArabicSlice';
-import { getSelectedSura } from '@/entities/Surah';
-import { getSelectedPage } from '@/entities/Page';
-import { useAppDispatch } from '@/shared/lib/hooks/useAppDispatch/useAppDispatch';
-import BookBoxSkeleton from '@/shared/ui/BookBoxSkeleton/BookBoxSkeleton';
-import BookBox from '@/shared/ui/BookBox/BookBox';
-import ReadingQuranErrorDialog from '@/shared/ui/ErrorDialog/ErrorDialog';
-import { fetchReadingArabic } from '../model/services/fetchReadingArabic';
+// import { getSelectedSura } from '@/entities/Surah';
+// import { getSelectedPage } from '@/entities/Page';
+// import { useAppDispatch } from '@/shared/lib/hooks/useAppDispatch/useAppDispatch';
+// import BookBoxSkeleton from '@/shared/ui/BookBoxSkeleton/BookBoxSkeleton';
+// import ReadingQuranErrorDialog from '@/shared/ui/ErrorDialog/ErrorDialog';
+// import { fetchReadingArabic } from '../model/services/fetchReadingArabic';
 
 interface ReadingArabicProps {
   className?: string;
@@ -30,47 +29,47 @@ const reducers: ReducersList = {
 };
 
 export const ReadingArabic = memo(({ className }: ReadingArabicProps) => {
-  const dispatch = useAppDispatch();
-  const currentSura = useSelector(getSelectedSura);
-  const currentPage = useSelector(getSelectedPage);
+  // const dispatch = useAppDispatch();
+  // const currentSura = useSelector(getSelectedSura);
+  // const currentPage = useSelector(getSelectedPage);
 
-  const data = useSelector(getReadingArabicData);
-  const isLoading = useSelector(getReadingArabicIsLoading);
-  const isError = useSelector(getReadingArabicError);
+  // const data = useSelector(getReadingArabicData);
+  // const isLoading = useSelector(getReadingArabicIsLoading);
+  // const isError = useSelector(getReadingArabicError);
 
-  useEffect(() => {
-    if (currentSura?.quran_order && data && !data[currentSura?.quran_order]) {
-      dispatch(
-        fetchReadingArabic({
-          suraId: currentSura?.quran_order,
-          pageNumber: currentPage.pageNumber,
-          limitOfPage: 1,
-        }),
-      );
-    } else if (currentSura?.quran_order && !data) {
-      dispatch(
-        fetchReadingArabic({
-          suraId: currentSura?.quran_order,
-          pageNumber: currentPage.pageNumber,
-          limitOfPage: 1,
-        }),
-      );
-    }
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [currentSura?.quran_order, dispatch]);
+  // useEffect(() => {
+  //   if (currentSura?.quran_order && data && !data[currentSura?.quran_order]) {
+  //     dispatch(
+  //       fetchReadingArabic({
+  //         suraId: currentSura?.quran_order,
+  //         pageNumber: currentPage.pageNumber,
+  //         limitOfPage: 1,
+  //       }),
+  //     );
+  //   } else if (currentSura?.quran_order && !data) {
+  //     dispatch(
+  //       fetchReadingArabic({
+  //         suraId: currentSura?.quran_order,
+  //         pageNumber: currentPage.pageNumber,
+  //         limitOfPage: 1,
+  //       }),
+  //     );
+  //   }
+  //   // eslint-disable-next-line react-hooks/exhaustive-deps
+  // }, [currentSura?.quran_order, dispatch]);
 
-  useEffect(() => {
-    if (currentSura?.quran_order) {
-      dispatch(
-        fetchReadingArabic({
-          suraId: currentSura?.quran_order,
-          pageNumber: currentPage.pageNumber,
-          limitOfPage: 1,
-        }),
-      );
-    }
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [currentPage, dispatch]);
+  // useEffect(() => {
+  //   if (currentSura?.quran_order) {
+  //     dispatch(
+  //       fetchReadingArabic({
+  //         suraId: currentSura?.quran_order,
+  //         pageNumber: currentPage.pageNumber,
+  //         limitOfPage: 1,
+  //       }),
+  //     );
+  //   }
+  //   // eslint-disable-next-line react-hooks/exhaustive-deps
+  // }, [currentPage, dispatch]);
 
   return (
     <DynamicModuleLoader reducers={reducers} removeAfterUnmount={false}>
@@ -81,24 +80,7 @@ export const ReadingArabic = memo(({ className }: ReadingArabicProps) => {
         <div
           className={classNames(cls.ReadingArabic__readBox, {}, [className])}
         >
-          {isLoading ? (
-            <BookBoxSkeleton />
-          ) : data && data[currentSura?.quran_order]?.data.resourse ? (
-            <BookBox
-              imgUrl={`${
-                data[currentSura?.quran_order]?.data.resourse[
-                  currentPage.pageNumber - 1
-                ]
-              }`}
-            />
-          ) : isError ? (
-            <ReadingQuranErrorDialog
-              isErrorProps={!false}
-              errorProps={isError}
-            />
-          ) : (
-            ''
-          )}
+          {}
         </div>
       </div>
     </DynamicModuleLoader>
